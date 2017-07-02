84dbbd456fe288c736573e51015ca7b4e32debc4
bgsrb <bgsrb88@gmail.com>
2017-07-02 12:57:47 +0430

    nuget update

---
+++ KavenegarExamplesCsharpWebApplication/KavenegarExamplesCsharpWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using  Kavenegar;
+using Kavenegar.Exceptions;
+
+namespace KavenegarExamplesCsharpWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            try
+            {
+                var api = new KavenegarApi("Your Api Key");
+                var result = api.Send("", "09361234567", "خدمات پیام کوتاه کاوه نگار");
+                return View(result);
+            }
+            catch (ApiException ex)
+            {
+                // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
+                Console.Write("Message : " + ex.Message);
+            }
+            catch (HttpException ex)
+            {
+                // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+                Console.Write("Message : " + ex.Message);
+            }
+            return View();
+        }
+    }
+}
+
